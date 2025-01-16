ab6f2d474a5df991fa20e032436852e3ea827836
gahson <kacpergarus12@gmail.com>
2025-01-16 02:14:48 +0100

    d5 done

--- 2024/Day5/d5.py
+++ 2024/Day5/d5.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 rules = []
 pages = []
 with open('data.txt', 'r') as file:
@@ -14,7 +15,7 @@ for i in range(len(filelines)):
 for i in range(idx, len(filelines)):
     pages.append(list(map(int, filelines[i].split(','))))
 
-rule_list = [[]for i in range(100)]
+rule_list = defaultdict(list)
 for lr, rr in rules:
     rule_list[lr].append(rr)
 
@@ -26,16 +27,46 @@ for page in pages:
     for i in range(1,n):
         for b in bef:
             if b in rule_list[page[i]]:
-                pages2.append(page)
+                if page not in pages2:
+                    pages2.append(page)
                 break
         else:
             bef.append(page[i])
     if len(bef) == n:
         res += page[n//2]
+    
 
 print(res)
 
 # second star excersise
+res2 = 0
+def topological_sort(graph, upd):
+    visited = set()
+    restored_page = []
+
+    def dfs(node):
+        visited.add(node)
+        for n in graph[node]:
+            if n not in visited:
+                dfs(n)
+        restored_page.append(node)
+    
+    for u in upd:
+        if u not in visited:
+            dfs(u)
+
+    return restored_page[::-1]
 
 for page in pages2:
-    pass
+    rule_list = defaultdict(list) 
+    
+    for lr, rr in rules:
+        if lr in page and rr in page:
+            
+            rule_list[lr].append(rr)
+    
+    sorted_page = topological_sort(rule_list, page)
+    res2 += sorted_page[len(sorted_page)//2]
+
+            
+print(res2)

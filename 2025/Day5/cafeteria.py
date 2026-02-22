fbd26ba0585805067fb5bd59bf46b412e643a03e
gahson <kacpergarus12@gmail.com>
2026-02-22 23:01:43 +0100

    d5 p1

---
+++ 2025/Day5/cafeteria.py
@@ -0,0 +1,54 @@
+def cafeteria(ranges, ids):
+    merged_ranges = find_max_range(ranges)
+    s = 0
+    n = len(merged_ranges)
+    
+    for person_id in ids:
+        low = 0
+        high = n - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            start, end = merged_ranges[mid]
+            
+            if start <= person_id <= end:
+                s += 1
+                break
+            elif person_id < start:
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+    return s
+
+def find_max_range(ranges):
+
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+
+    merged = [sorted_ranges[0]]
+
+    for current in sorted_ranges[1:]:
+        last_merged = merged[-1]
+
+        if current[0] <= last_merged[1]:
+            last_merged[1] = max(last_merged[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged
+
+
+
+
+
+
+
+
+with open("data5.txt", "r",  encoding="utf-8") as f:
+    ranges, ids = f.read().strip().split("\n\n")
+    ranges = ranges.split("\n")
+    ranges = [list(map(int, r.split("-"))) for r in ranges]
+    ids = list(map(int, ids.split("\n")))
+
+print(cafeteria(ranges, ids))
+

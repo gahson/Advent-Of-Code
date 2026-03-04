4b615e347cc572aa35d05d645f6a527688cab668
gahson <kacpergarus12@gmail.com>
2026-03-04 02:58:31 +0100

    d7 p2

--- 2025/Day7/Laboratories.py
+++ 2025/Day7/Laboratories.py
@@ -18,10 +18,31 @@ def lab(data):
         streams -= to_pop
     return s
 
+def lab_2(data):
+    start_col = 0
+    for i, char in enumerate(data[0]):
+        if char == 'S':
+            start_col = i
+            break
+
+    current_paths = {start_col: 1}
+
+    for l in data:
+        next_paths = {}
+        for col, count in current_paths.items():
+            if l[col] == "^":
+                next_paths[col - 1] = next_paths.get(col - 1, 0) + count
+                next_paths[col + 1] = next_paths.get(col + 1, 0) + count
+            else:
+                next_paths[col] = next_paths.get(col, 0) + count
+
+        current_paths = next_paths
+
+    return sum(current_paths.values())
 
 
 data = []
 with open("data7.txt", "r", encoding = "utf - 8") as f:
     for l in f:
         data.append(list(l.strip()))
-print(lab(data))
+print(lab_2(data))

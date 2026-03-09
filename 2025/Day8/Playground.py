e3d168e89778ec97bab11dd83bf2412a449697d7
gahson <kacpergarus12@gmail.com>
2026-03-09 23:45:57 +0100

    d8 p2

--- 2025/Day8/Playground.py
+++ 2025/Day8/Playground.py
@@ -48,10 +48,49 @@ def playg(data):
     return result
 
 
+def playg2(data):
+    num_boxes = len(data)
+    edges = []
+
+    for i, j in combinations(range(num_boxes), 2):
+        p1, p2 = data[i], data[j]
+        dist = math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 + (p1[2] - p2[2]) ** 2)
+        edges.append((dist, i, j))
+
+    edges.sort()
+
+    parent = {i: i for i in range(num_boxes)}
+    size = {i: 1 for i in range(num_boxes)}
+
+    def find(i):
+        if parent[i] == i:
+            return i
+        parent[i] = find(parent[i])
+        return parent[i]
+
+    for edge in edges:
+        dist, i, j = edge
+
+        root_i = find(i)
+        root_j = find(j)
+
+        if root_i != root_j:
+            if size[root_i] < size[root_j]:
+                root_i, root_j = root_j, root_i
+            parent[root_j] = root_i
+            size[root_i] += size[root_j]
+
+            if size[root_i] == num_boxes:
+                x1 = data[i][0]
+                x2 = data[j][0]
+                return x1 * x2
+
+
+
 data = []
 with open("data8.txt", "r", encoding="utf-8") as f:
     for l in f:
         l = list(map(int, l.strip().split(",")))
         data.append(l)
 
-print(playg(data))
+print(playg2(data))
